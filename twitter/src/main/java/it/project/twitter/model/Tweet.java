e61073cde373287b13e82b80643fcb1a5066a4ff
Mattia Scuriatti <S1090144@studenti.univpm.it>
2020-06-03 16:27:00 +0200

    Reload Tweet,MetaData,UserMention

--- twitter/src/main/java/it/project/twitter/model/Tweet.java
+++ twitter/src/main/java/it/project/twitter/model/Tweet.java
@@ -1,5 +1,62 @@
 package it.project.twitter.model;
 
-public class Tweet {
+import java.util.ArrayList;
 
+public class Tweet {
+     private String id = "";
+     private String idAuthor = "";
+     private String dataCreation = "";
+     private String lang = "";
+     private String source = "";
+     private String text = "";
+     private ArrayList<String> userMentions = new ArrayList<String>();
+     
+	public String getId() {
+		return id;
+	}
+	public void setId(String new_id) {
+		this.id = new_id;
+	}
+	public String getIdAuthor() {
+		return idAuthor;
+	}
+	public void setIdAuthor(String new_idAuthor) {
+		this.idAuthor = new_idAuthor;
+	}
+	public String getDataCreation() {
+		return dataCreation;
+	}
+	public void setDataCreation(String new_dataCreation) {
+		this.dataCreation = new_dataCreation;
+	}
+	public String getLang() {
+		return lang;
+	}
+	public void setLang(String new_lang) {
+		this.lang = new_lang;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String new_source) {
+		this.source = new_source;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String new_text) {
+		this.text = new_text;
+	}
+	public ArrayList<String> getUserMentions() {
+		return userMentions;
+	}
+	
+	public void setUserMentions(String u) {
+		{
+			if(!(userMentions.contains(u)))
+				   userMentions.add(u);
+		}
+	}
+     
+     
 }
--- twitter/src/main/java/it/project/twitter/model/UserMention.java
+++ twitter/src/main/java/it/project/twitter/model/UserMention.java
@@ -1,5 +1,37 @@
 package it.project.twitter.model;
 
 public class UserMention {
+	private String ScreenName;
+	private String name;
+	private int Id_usermention;
+	
+	
+	public UserMention() {
+		super();
+	}
+	
+	public String getScreenName() {
+		return ScreenName;
+	}
+	
+	public void setScreenName(String screenName) {
+		ScreenName = screenName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    public int getId_usermention() {
+		return Id_usermention;
+	}
+
+	public void setId_usermention(int Id_usermention) {
+		this.Id_usermention = Id_usermention;
+	}
+	
+	
 
 }
