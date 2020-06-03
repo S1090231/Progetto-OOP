e3d7a6896a843c8029a0affaf61c8ff013d04af1
Giada Gatti <s1090231@studenti.univpm.it>
2020-06-03 17:26:47 +0100

    Added routes for tweets, metadata and related methods in TweetService

--- twitter/src/main/java/it/project/twitter/controller/ControllerClass.java
+++ twitter/src/main/java/it/project/twitter/controller/ControllerClass.java
@@ -1,5 +1,35 @@
 package it.project.twitter.controller;
 
+import org.hibernate.validator.cfg.context.ReturnValueConstraintMappingContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.project.twitter.service.TweetService;
+
+@RestController
 public class ControllerClass {
+	
+	@Autowired
+	private TweetService tweetservice;
+	
+	@RequestMapping(value="/metadata", method = RequestMethod.GET)
+	public ResponseEntity<Object> getMetadatas() {
+		return new ResponseEntity<>(tweetservice.getMetada(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/tweet", method = RequestMethod.GET)
+	public ResponseEntity<Object> getTweets() {
+		return new ResponseEntity<>(tweetservice.getTweet(), HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
 
 }
--- twitter/src/main/java/it/project/twitter/service/TweetService.java
+++ twitter/src/main/java/it/project/twitter/service/TweetService.java
@@ -2,4 +2,8 @@ package it.project.twitter.service;
 
 public interface TweetService {
 
+	Object getMetada();
+
+	Object getTweet();
+
 }
