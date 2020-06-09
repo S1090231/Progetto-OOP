29e88365f4b7061df8c1ab1ca40a1a00797f6a8c
Mattia Scuriatti <S1090144@studenti.univpm.it>
2020-06-09 18:56:27 +0200

    Small fixes

--- twitter/src/main/java/it/project/twitter/service/TweetService.java
+++ twitter/src/main/java/it/project/twitter/service/TweetService.java
@@ -1,9 +1,13 @@
 package it.project.twitter.service;
 
+import java.util.Collection;
+
+import it.project.twitter.model.Metadata;
+import it.project.twitter.model.Tweet;
+
 public interface TweetService {
 
-	Object getMetada();
-
-	Object getTweet();
+	public abstract Collection<Metadata> getMetada();
+    public abstract Collection<Tweet> getTweet();
 
 }
--- twitter/src/main/java/it/project/twitter/service/TweetServiceImplementation.java
+++ twitter/src/main/java/it/project/twitter/service/TweetServiceImplementation.java
@@ -1,5 +1,33 @@
 package it.project.twitter.service;
 
+import java.security.KeyStore.TrustedCertificateEntry;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import it.project.twitter.model.Metadata;
+import it.project.twitter.model.Tweet;
+
 public class TweetServiceImplementation implements TweetService {
+	
+	private static Map<Integer,Tweet> alltweets = new HashMap<>();
+	private static Map<String, Metadata> Metadata = new HashMap<>();
+	
+  
+
+	@Override
+	public Collection<Metadata> getMetada() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Tweet> getTweet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
