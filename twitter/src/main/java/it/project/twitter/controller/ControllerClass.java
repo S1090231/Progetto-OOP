6ea781ac39a5532f0449597b9de1cbc3b3299b8d
Giada Gatti <s1090231@studenti.univpm.it>
2020-06-16 13:30:00 +0100

    New route created in the controller

--- twitter/src/main/java/it/project/twitter/controller/ControllerClass.java
+++ twitter/src/main/java/it/project/twitter/controller/ControllerClass.java
@@ -4,6 +4,7 @@ package it.project.twitter.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +25,12 @@ public class ControllerClass {
 	@RequestMapping(value="/tweet", method = RequestMethod.GET)
 	public ResponseEntity<Object> getTweets() {
 		return new ResponseEntity<>(tweetservice.getTweet(), HttpStatus.OK);
-	}
+		}
 	
+	@RequestMapping(value="/tweet", method = RequestMethod.POST)
+	public ResponseEntity<Object> getFilteredTweets(@RequestBody Object JsonFilter) {
+		return new ResponseEntity<>();
+		}
 	
 	
 	
